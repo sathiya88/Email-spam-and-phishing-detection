0f62a6a1c0c8a172ac679d9bc2af003068a0b71b
Sathiya Pandi D <147373264+sathiya88@users.noreply.github.com>
2025-08-17 19:00:22 +0530

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import joblib
+
+# Load saved model & vectorizer
+model = joblib.load('model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+label_map = {0: 'Legitimate', 1: 'Spam', 2: 'Phishing'}
+
+st.title("📧 Email Spam & Phishing Detector")
+
+email_text = st.text_area("Paste your email content here:")
+if st.button("Predict"):
+    X_input = vectorizer.transform([email_text.lower()])
+    pred = model.predict(X_input)[0]
+    st.success(f"Prediction: {label_map[pred]}")
